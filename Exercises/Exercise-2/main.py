0917431d97b53712e7d1091a3f67c2b23098e521
Marián Ragan <ragan.marian@gmail.com>
2023-04-13 18:13:13 +0200

    Start exercise 2

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,12 +1,31 @@
 import requests
-import pandas
+# import pandas
 
+URL = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/'
+TIMESTAMP = '2022-02-07 14:03'
 
-
-def main():
-    # your code here
-    pass
+def main(URL):
+    r = requests.get(URL)
+    print(r.status_code)
+    print(r.encoding)
+    return r.text
+    
 
 
 if __name__ == '__main__':
-    main()
+    # m = main(URL)
+    # with open('data.txt', 'w') as f:
+    #     f.write(m)
+
+    file = open('data.txt', 'r')
+    
+    for line in file:
+        # print(line)
+        # print(type(line))
+        if TIMESTAMP in line:
+            print(line)
+            break
+        continue
+
+    file.close()
+    # print(type(m))
